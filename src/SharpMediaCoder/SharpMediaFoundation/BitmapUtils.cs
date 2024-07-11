0cc38179fafd649bc56f9070df5616e30eda12f7
Lukas Volf <jimm98y@hotmail.com>
2024-07-11 23:36:58 +0200

    F Added managed image copy

--- src/SharpMediaCoder/SharpMediaFoundation/BitmapUtils.cs
+++ src/SharpMediaCoder/SharpMediaFoundation/BitmapUtils.cs
@@ -1,10 +1,11 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace SharpMediaFoundation
 {
     public static class BitmapUtils
     {
-        public static void CopyBitmap(byte[] decoded, nint backBuffer, int originalWidth, int originalHeight, int decodedWidth, int decodedHeight, bool flip = true)
+        public static void CopyBitmap(byte[] source, nint dest, int originalWidth, int originalHeight, int decodedWidth, int decodedHeight, bool flip = true)
         {
             int decodedStride = decodedWidth * 3;
             int startIndex = (decodedHeight - originalHeight) * decodedStride;
@@ -14,9 +15,28 @@ namespace SharpMediaFoundation
             for (int i = 0; i < originalHeight; i++)
             {
                 Marshal.Copy(
-                   decoded,
+                   source,
                    startIndex + i * decodedStride,
-                   backBuffer + wbIndex + i * wbFlip * wbStride,
+                   dest + wbIndex + i * wbFlip * wbStride,
+                   wbStride
+                );
+            }
+        }
+
+        public static void CopyBitmap(byte[] source, byte[] dest, int originalWidth, int originalHeight, int decodedWidth, int decodedHeight, bool flip = true)
+        {
+            int decodedStride = decodedWidth * 3;
+            int startIndex = (decodedHeight - originalHeight) * decodedStride;
+            int wbStride = originalWidth * 3;
+            int wbIndex = flip ? wbStride * (originalHeight - 1) : 0;
+            int wbFlip = flip ? -1 : 1;
+            for (int i = 0; i < originalHeight; i++)
+            {
+                Buffer.BlockCopy(
+                   source,
+                   startIndex + i * decodedStride,
+                   dest,
+                   wbIndex + i * wbFlip * wbStride,
                    wbStride
                 );
             }
